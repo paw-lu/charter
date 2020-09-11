411a366f5ac4d01b8db40df3001093182463eb6e
Paulo S. Costa <pauloscosta5@gmail.com>
2020-09-10 20:26:58 -0700

    Refactor axis into dataclass

--- src/charter/axis.py
+++ src/charter/axis.py
@@ -1,10 +1,12 @@
 """A unicode number line."""
 import bisect
+import dataclasses
 import math
 from typing import List
 from typing import Tuple
 
 
+@dataclasses.dataclass()
 class Ticks:
     """Chart ticks.
 
@@ -16,149 +18,219 @@ class Ticks:
         max_ticks (int): The maximum number of ticks.
 
     Attributes:
-        tick_positions (List[float]): The positions of
-            the ticks in ascending order.
-        tick_labels (List[str]): The tick labels in ascending order.
+        tick_values (List[float]): The values of the ticks in ascending
+            order.
+        axis_power (int): The power of ten in a number.
+        axis_subtractor (float): The number which will be subtracted
+            from all axis values for display.
+        tick_divisor_power (int): The power of ten all tick values will
+            be divided by after being subtracted by the
+            ``axis_subtractor.``
+        tick_labels (List[str]): The tick labels in order.
+        axis_subtractor_label (str): The axis subtractor label.
     """
 
-    def __init__(self, min_data: float, max_data: float, max_ticks: int) -> None:
+    min_data: float
+    max_data: float
+    max_ticks: int
+
+    def __post_init__(self) -> None:
         """Constructor."""
-        self.min_data = min_data
-        self.max_data = max_data
-        self.max_ticks = max_ticks
+        self.tick_values = _get_tick_values(
+            min_data=self.min_data, max_data=self.max_data, max_ticks=self.max_ticks
+        )
+        self.axis_power = _find_closest_prefix_power(self.tick_values[-1])
+        self.axis_subtractor, self.tick_divisor_power = _get_axis_label_adjustors(
+            self.tick_values
+        )
+        self.tick_labels = _make_tick_labels(
+            self.tick_values,
+            axis_subtractor=self.axis_subtractor,
+            tick_divisor_power=self.tick_divisor_power,
+        )
+        self.axis_subtractor_label = (
+            None
+            if self.axis_subtractor == 0
+            else (
+                f"{self.axis_subtractor / self.axis_power:0.2f}"
+                f"{_get_metric_prefix(self.axis_power)}"
+            )
+        )
         pass
 
-    def __repr__(self) -> str:
-        """Representation of the Tick class."""
-        return (
-            f"Ticks(tick_positions={tuple(self.tick_positions)},"
-            f" tick_labels={tuple(self.tick_labels)})"
-        )
 
-    def _find_closest_prefix_power(self, number: float) -> int:
-        """Return the closest power associated with an SI prefix.
+def _find_closest_prefix_power(number: float) -> int:
+    """Return the closest power associated with an SI prefix.
 
-        Args:
-            number (float): A number to find the prefix power
-                for.
+    Args:
+        number (float): A number to find the prefix power
+            for.
 
-        Returns:
-            int: The closest SI prefix power.
-        """
-        return max(min(int(math.log10(number) // 3 * 3), 24), -24)
+    Returns:
+        int: The closest SI prefix power.
+    """
+    return max(min(math.floor(math.log10(number)) // 3 * 3, 24), -24)
 
-    @property
-    def tick_labels(self) -> List[str]:
-        """Given a collection of ticks, return a formatted version.
 
-        Returns:
-            Generator[str, None, None]: The generated tick labels.
-        """
-        metric_prefix = {
-            24: "Y",
-            21: "Z",
-            18: "E",
-            15: "P",
-            12: "T",
-            9: "G",
-            6: "M",
-            3: "k",
-            -3: "m",
-            -6: "μ",
-            -9: "n",
-            -12: "p",
-            -15: "f",
-            -18: "a",
-            -21: "z",
-            -24: "y",
-        }
-        first_tick_position = self.tick_positions[0]
-        second_tick_position = self.tick_positions[1]
-        step_size = second_tick_position - first_tick_position
-        axis_divisor_power = self._find_closest_prefix_power(self.tick_positions[-1])
-        axis_prefix = metric_prefix.get(axis_divisor_power, "")
-        step_place = math.log10(step_size)
-        number_of_ticks = len(self.tick_positions)
-        if 2 < (axis_divisor_power - math.floor(step_place)):
-            tick_divisor_power = self._find_closest_prefix_power(step_size)
-            tick_divisor_prefix = metric_prefix.get(tick_divisor_power, "")
-            return [
-                f"{(tick - 10 ** axis_divisor_power) / 10 ** tick_divisor_power:0.2f}"
-                f"{tick_divisor_prefix}"
-                + (i == number_of_ticks - 1) * f" + 1{axis_prefix}"
-                for i, tick in enumerate(self.tick_positions)
-            ]
-        else:
-            return [
-                f"{tick / 10 ** axis_divisor_power :0.2f}"
-                + (i == number_of_ticks - 1) * axis_prefix
-                for i, tick in enumerate(self.tick_positions)
-            ]
+def _get_axis_label_adjustors(tick_values: List[float]) -> Tuple[float, int]:
+    """Return the tick divisor power and axis subtractor.
 
-    def _round_number(
-        self,
-        number: float,
-        limits: Tuple[float, ...],
-        rounding_terms: Tuple[float, ...],
-        allow_equal: bool,
-    ) -> float:
-        """Round a number to a intuitive form.
+    All ticks labels will be subtracted by the axis subtractor, then
+    divided by ten to the tick divisor power.
 
-        Args:
-            number (float): The number to round
-            limits (Tuple[float, ...]): The associated rounding points
-                in ascending order. The rounding term assigned to the
-                number depends on where it is placed on the it will be
-                rounded to the associated rounding points on
-                ``rounding_terms``.
-            rounding_terms (Tuple[float, ...]): The terms to round to if
-                the number falls below or equal to the associated
-                rounding points.
-            allow_equal (bool): Whether to allow the number to be equal
-                to the rounding points. If set False
+    Args:
+        tick_values (List[float]): The tick values in ascending order.
 
-        Returns:
-            float: The number in a rounded form.
-        """
-        power = math.floor(math.log10(number))
-        leading_term = number / 10 ** power
-        limit_index = (
-            bisect.bisect_left(limits, leading_term)
-            if allow_equal
-            else bisect.bisect_right(limits, leading_term)
-        )
-        rounded_lead = (
-            10 if (len(limits) - 1) < limit_index else rounding_terms[limit_index]
-        )
-        return rounded_lead * 10.0 ** power
-
-    @property
-    def tick_positions(self) -> List[float]:
-        """Calculate the positions of the ticks.
-
-        Returns:
-            List[float]: The tick positions in ascending order.
-        """
-        if self.max_data == self.min_data:
-            return [self.min_data]
-        else:
-            data_range = self.max_data - self.min_data
-            rounded_range = self._round_number(
-                data_range,
-                limits=(1.5, 3, 7),
-                rounding_terms=(1, 2, 5),
-                allow_equal=False,
+    Returns:
+        (Tuple) Two element tuple containing
+        axis_subtractor (float): The number that will be subtracted
+            from all axis tick labels.
+        tick_divisor_power (int): The tens power to divide all tick
+            labels by.
+    """
+    number_of_ticks = len(tick_values)
+    axis_divisor_power = _find_closest_prefix_power(tick_values[-1])
+    if number_of_ticks <= 1:
+        tick_divisor_power = axis_divisor_power
+        axis_subtractor = 0
+    else:
+        step_size = tick_values[1] - tick_values[0]
+        step_place = math.floor(math.log10(step_size))
+        if 2 < (axis_divisor_power - step_place):
+            axis_range_place = math.floor(math.log10(tick_values[-1] - tick_values[0]))
+            tick_place = (
+                step_place if 2 < (axis_range_place - step_place) else axis_range_place
             )
-            spacing = rounded_range / (self.max_ticks - 1)
-            tick_spacing = self._round_number(
-                spacing, limits=(1, 2, 5), rounding_terms=(1, 2, 5), allow_equal=True
+            axis_subtractor = (tick_values[-1] // 10 ** (tick_place + 1)) * 10 ** (
+                tick_place + 1
             )
-            first_tick = math.floor(self.min_data / tick_spacing) * tick_spacing
-            last_tick = math.ceil(self.max_data / tick_spacing) * tick_spacing
-            number_of_ticks: int = math.ceil(
-                (last_tick - first_tick) / tick_spacing
-            ) + 1
-            return [
-                first_tick + factor * tick_spacing
-                for factor in range(0, number_of_ticks)
-            ]
+            tick_divisor_power = _find_closest_prefix_power(10 ** tick_place)
+        else:
+            tick_divisor_power = axis_divisor_power
+            axis_subtractor = 0
+    return axis_subtractor, tick_divisor_power
+
+
+def _get_metric_prefix(power: int, default: str = "") -> str:
+    """Return the metric prefix for the power.
+
+    Args:
+        power (int): The power whose metric prefix will be returned.
+        default (str): The default value to return if an exact match is
+            not found.
+
+    Returns:
+        str: The metric prefix.
+    """
+    metric_prefix = {
+        24: "Y",
+        21: "Z",
+        18: "E",
+        15: "P",
+        12: "T",
+        9: "G",
+        6: "M",
+        3: "k",
+        -3: "m",
+        -6: "μ",
+        -9: "n",
+        -12: "p",
+        -15: "f",
+        -18: "a",
+        -21: "z",
+        -24: "y",
+    }
+    return metric_prefix.get(power, default)
+
+
+def _make_tick_labels(
+    tick_values: List[float], axis_subtractor: float, tick_divisor_power: int,
+) -> List[str]:
+    """Given a collection of ticks, return a formatted version.
+
+    Args:
+        tick_values (List[float]): The ticks positions in ascending
+            order.
+        tick_divisor_power (int): The power of ten the tick labels will
+            be divided by.
+        axis_subtractor (float): The amount to subtract from the tick
+            values.
+
+    Returns:
+        Generator[str, None, None]: The generated tick labels.
+    """
+    tick_divisor_prefix = _get_metric_prefix(tick_divisor_power)
+    return [
+        f"{(tick - axis_subtractor) / 10 ** tick_divisor_power:0.2f}"
+        f"{tick_divisor_prefix}"
+        for tick in tick_values
+    ]
+
+
+def _round_number(
+    number: float,
+    limits: Tuple[float, ...],
+    rounding_terms: Tuple[float, ...],
+    allow_equal: bool,
+) -> float:
+    """Round a number to an intuitive form.
+
+    Args:
+        number (float): The number to round
+        limits (Tuple[float, ...]): The associated rounding points
+            in ascending order. The rounding term assigned to the
+            number depends on where it is placed on the it will be
+            rounded to the associated rounding points on
+            ``rounding_terms``.
+        rounding_terms (Tuple[float, ...]): The terms to round to if
+            the number falls below or equal to the associated
+            rounding points.
+        allow_equal (bool): Whether to allow the number to be equal
+            to the rounding points. If set False
+
+    Returns:
+        float: The number in a rounded form.
+    """
+    power = math.floor(math.log10(number))
+    leading_term = number / 10 ** power
+    limit_index = (
+        bisect.bisect_left(limits, leading_term)
+        if allow_equal
+        else bisect.bisect_right(limits, leading_term)
+    )
+    rounded_lead = (
+        10 if (len(limits) - 1) < limit_index else rounding_terms[limit_index]
+    )
+    return rounded_lead * 10.0 ** power
+
+
+def _get_tick_values(min_data: float, max_data: float, max_ticks: int) -> List[float]:
+    """Calculate the positions of the ticks.
+
+    Args:
+        min_data (float): The minimum value of the data for the axis
+                dimension.
+        max_data (float): The maximum value of the data for the axis
+            dimension.
+        max_ticks (int): The maximum number of ticks.
+
+    Returns:
+        List[float]: The tick positions in ascending order.
+    """
+    if max_data == min_data:
+        return [min_data]
+    else:
+        data_range = max_data - min_data
+        rounded_range = _round_number(
+            data_range, limits=(1.5, 3, 7), rounding_terms=(1, 2, 5), allow_equal=False,
+        )
+        spacing = rounded_range / (max_ticks - 1)
+        tick_spacing = _round_number(
+            spacing, limits=(1, 2, 5), rounding_terms=(1, 2, 5), allow_equal=True
+        )
+        first_tick = math.floor(min_data / tick_spacing) * tick_spacing
+        last_tick = math.ceil(max_data / tick_spacing) * tick_spacing
+        number_of_ticks: int = math.ceil((last_tick - first_tick) / tick_spacing) + 1
+        return [
+            first_tick + factor * tick_spacing for factor in range(0, number_of_ticks)
+        ]
--- tests/test_axis.py
+++ tests/test_axis.py
@@ -1,4 +1,5 @@
 """Test the number line generator."""
+from typing import List
 from typing import Tuple
 
 import pytest
@@ -6,12 +7,22 @@ import pytest
 from charter import axis
 
 
-def test_ticks_repr() -> None:
-    """It returns a string representation of ``Ticks``."""
-    assert str(axis.Ticks(min_data=1, max_data=3, max_ticks=5)) == (
-        "Ticks(tick_positions=(1.0, 1.5, 2.0, 2.5, 3.0),"
-        " tick_labels=('1.00', '1.50', '2.00', '2.50', '3.00'))"
-    )
+def test_tick_labels() -> None:
+    """It generates tick labels."""
+    ticks = axis.Ticks(min_data=123456 + 1, max_data=123456 + 11, max_ticks=14)
+    assert ticks.tick_labels == [
+        "57.00",
+        "58.00",
+        "59.00",
+        "60.00",
+        "61.00",
+        "62.00",
+        "63.00",
+        "64.00",
+        "65.00",
+        "66.00",
+        "67.00",
+    ]
 
 
 @pytest.mark.parametrize(
@@ -30,9 +41,8 @@ def test__round_number(
     expected_rounded_num: float,
 ) -> None:
     """It rounds the number."""
-    ticks = axis.Ticks(min_data=1, max_data=4, max_ticks=10)
     assert (
-        ticks._round_number(
+        axis._round_number(
             number=number,
             limits=limits,
             rounding_terms=rounding_terms,
@@ -42,10 +52,12 @@ def test__round_number(
     )
 
 
-def test_tick_positions_min_max_equal() -> None:
+def test_tick_values_min_max_equal() -> None:
     """It returns equal min and maximum ticks when bounds are equal."""
-    ticks = axis.Ticks(min_data=10, max_data=10, max_ticks=20)
-    assert ticks.tick_positions == [10.0]
+    actual_tick_values = axis._get_tick_values(
+        min_data=10.0, max_data=10.0, max_ticks=10
+    )
+    assert actual_tick_values == [10.0]
 
 
 @pytest.mark.parametrize(
@@ -57,40 +69,96 @@ def test_tick_positions_min_max_equal() -> None:
         (1 + 1e-7, 1 + 5e-7, 10, [1 + (1e-7 * factor) for factor in range(1, 7)]),
     ],
 )
-def test_tick_positions(
+def test__get_tick_values(
     min_data: float,
     max_data: float,
     max_ticks: int,
     expected_ticks: Tuple[float, float, float],
 ) -> None:
     """It creates ticks."""
-    ticks = axis.Ticks(min_data=min_data, max_data=max_data, max_ticks=max_ticks)
-    assert ticks.tick_positions == pytest.approx(expected_ticks)
-
-
-@pytest.mark.parametrize("number, expected_power", [(1e3, 3), (200e6, 6), (100e-3, -3)])
-def test__find_closest_prefix_power(number: float, expected_power: int) -> None:
-    """It finds the closest power associated to an SI prefix."""
-    ticks = axis.Ticks(min_data=1, max_data=4, max_ticks=10)
-    assert expected_power == ticks._find_closest_prefix_power(number)
+    actual_tick_values = axis._get_tick_values(
+        min_data=min_data, max_data=max_data, max_ticks=max_ticks
+    )
+    assert actual_tick_values == pytest.approx(expected_ticks)
 
 
 @pytest.mark.parametrize(
-    "min_data, max_data, max_ticks, expected_tick_labels",
+    "number, expected_power", [(1e3, 3), (200e6, 6), (0.1, -3), (1e-3, -3)]
+)
+def test__find_closest_prefix_power(number: float, expected_power: int) -> None:
+    """It finds the closest power associated to an SI prefix."""
+    actual_power = axis._find_closest_prefix_power(number=number)
+    assert actual_power == expected_power
+
+
+@pytest.mark.parametrize(
+    "tick_values, axis_subtractor, tick_divisor_power, expected_tick_labels",
     [
-        (1, 3, 3, ("1.00", "2.00", "3.00")),
-        (3.4, 10, 5, ("2.00", "4.00", "6.00", "8.00", "10.00")),
-        (3.4e3, 10e3, 5, ("2.00", "4.00", "6.00", "8.00", "10.00k"),),
-        (10e-8, 10e-6, 3, ("0.00", "5.00", "10.00", "15.00μ")),
-        (1_000_003, 1_000_005, 3, ("3.00", "4.00", "5.00 + 1M")),
-        (1_000_300_000, 1_000_500_000, 3, ("300.00k", "400.00k", "500.00k + 1G"),),
-        (1.00001, 1.00003, 3, ("10.00μ", "20.00μ", "30.00μ + 1")),
+        (
+            [1.00, 1.20, 1.40, 1.60, 1.80, 2.00],
+            0,
+            0,
+            ["1.00", "1.20", "1.40", "1.60", "1.80", "2.00"],
+        ),
+        ([1e6 + 3.00, 1e6 + 4.00, 1e6 + 5.00], 1e6, 0, ["3.00", "4.00", "5.00"]),
+        ([5_000_003], 0, 6, ["5.00M"]),
+        (
+            [5_561_943 + axis_tick for axis_tick in range(6)],
+            5561940,
+            0,
+            ["3.00", "4.00", "5.00", "6.00", "7.00", "8.00"],
+        ),
+        (
+            [1e9 + 300.00e3, 1e9 + 400.00e3, 1e9 + 500.00e3],
+            1e9,
+            3,
+            ["300.00k", "400.00k", "500.00k"],
+        ),
     ],
 )
-def test_tick_labels(
-    min_data: float, max_data: float, max_ticks: int, expected_tick_labels: Tuple[str]
+def test__make_tick_labels(
+    tick_values: List[float],
+    axis_subtractor: float,
+    tick_divisor_power: int,
+    expected_tick_labels: List[str],
 ) -> None:
     """It formats the ticks for display."""
-    axis_ticks = axis.Ticks(min_data=min_data, max_data=max_data, max_ticks=max_ticks)
-    tick_labels = tuple(axis_ticks.tick_labels)
-    assert tick_labels == expected_tick_labels
+    actual_tick_labels = axis._make_tick_labels(
+        tick_values=tick_values,
+        axis_subtractor=axis_subtractor,
+        tick_divisor_power=tick_divisor_power,
+    )
+    assert actual_tick_labels == expected_tick_labels
+
+
+@pytest.mark.parametrize(
+    "tick_values, expected_axis_subtractor, expected_tick_divisor_power",
+    [
+        ([1.00, 2.00, 3.00], 0, 0),
+        ([2.00, 4.00, 6.00, 8.00, 10.00], 0, 0),
+        ([2.00e3, 4.00e3, 6.00e3, 8.00e3, 10.00e3], 0, 3),
+        ([0.00e-6, 5.00e-6, 10.00e-6, 15.00e-6], 0, -6),
+        ([1.00, 1.20, 1.40, 1.60, 1.80, 2.00], 0, 0),
+        ([1e6 + 3.00, 1e6 + 4.00, 1e6 + 5.00], 1e6, 0),
+        ([1e6 + axis_tick / 10 for axis_tick in range(30, 50, 5)], 1e6, 0),
+        ([1e9 + 300.00e3, 1e9 + 400.00e3, 1e9 + 500.00e3], 1e9, 3),
+        ([1 + 10.00e-6, 1 + 20.00e-6, 1 + 30.00e-6], 1, -6),
+        ([axis_tick for axis_tick in range(500, 10_001, 280)], 0, 3),
+        ([axis_tick / 10 for axis_tick in range(10, 21, 1)], 0, 0),
+        ([100 + axis_tick * 1e-6 for axis_tick in range(1, 11)], 100, -6),
+        ([5_000_003], 0, 6),
+        ([5_561_943 + axis_tick for axis_tick in range(6)], 5561940, 0),
+    ],
+)
+def test__get_axis_label_adjustors(
+    tick_values: List[float],
+    expected_axis_subtractor: float,
+    expected_tick_divisor_power: int,
+) -> None:
+    """It finds the axis power and subtractor."""
+    actual_axis_subtractor, actual_tick_divisor_power = axis._get_axis_label_adjustors(
+        tick_values=tick_values
+    )
+    assert (actual_axis_subtractor, actual_tick_divisor_power) == pytest.approx(
+        (expected_axis_subtractor, expected_tick_divisor_power)
+    )
